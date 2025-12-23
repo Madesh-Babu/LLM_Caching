c8239db32518961d877cf5761a039423a5ce5f05
Madesh Babu <madeshbabu@bitcot.com>
2025-12-23 12:09:00 +0530

    calculated token and its cost

--- sqllite_cache.py
+++ sqllite_cache.py
@@ -1,6 +1,7 @@
 from langchain_openai import OpenAI
 from langchain_community.cache import SQLiteCache
 from langchain_core.globals import set_llm_cache
+from langchain_community.callbacks.manager import get_openai_callback
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -9,8 +10,11 @@ set_llm_cache(SQLiteCache(database_path="llm_cache.db"))
 
 llm = OpenAI(model_name="gpt-4o-mini", temperature=0)
 
-# print(llm.invoke("Explain about cricket"))
-# print(llm.invoke("Explain about cricket"))
-# print(llm.invoke("Explain about football"))
-
-print(llm.invoke("Explain about football"))
+propmt = "Explain about Kabaddi"
+with get_openai_callback() as cb:
+    response = llm.invoke(propmt)
+    print(f"Response: {response}")
+    print("-----------------------")
+    print(f"Tokens used: {cb.total_tokens}")
+    print("-----------------------")
+    print(f"Total Cost: ${cb.total_cost}")
